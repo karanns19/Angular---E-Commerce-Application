5846f0e6b63cb35ab4163db7e8dce03bc0e0e39e
karanns19 <karanns.aero19@gmail.com>
2022-05-17 20:00:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ihkq97
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ihkq97)
---
+++ src/app/products.ts
@@ -0,0 +1,33 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Iphone X',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+  },
+  {
+    id: 2,
+    name: 'OPPO A9',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+  },
+  {
+    id: 3,
+    name: 'Realme GT',
+    price: 299,
+    description: '',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
